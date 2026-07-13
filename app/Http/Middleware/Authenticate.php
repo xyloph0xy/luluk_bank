03e0d2b06b8385f8c1cec09c372b0d4076f1cf97
LulukSofiyah <sofiyahluluk7@gmail.com>
2026-07-14 03:53:17 +0700

    add middleware auth

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class Authenticate
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (! Auth::check()) {
+
+            return redirect()->route('login');
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [LoginController::class, 'redirectIfAuthenticated']);
 Route::get('login', [LoginController::class, 'login'])->name('login')->middleware('guest');
 Route::get('register', [RegisterController::class, 'register'])->name('register')->middleware('guest');
+
+Route::middleware('auth')->group(function () {
+
+    
+
+});
